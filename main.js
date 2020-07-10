5f016cdb5a1be617c6a82cea0030f9b4eeab8386
Ismail NGUYEN <Ismail.NGUYEN@loccitane.com>
2020-07-10 20:56:20 +0200

    Create a create element from scratch

---
+++ main.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function sayLouder(text) {
+	return text.toUpperCase();
+}
+
+function createElement() {
+	var element = React.createElement("div");
+	
+	return element;
+}
+
+console.log(createElement())
